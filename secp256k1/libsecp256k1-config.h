3f2c18e5c5447d622cc66ea921d13f27fcaa62f4
gotmyname2018 <gotmyname2018@outlook.com>
2019-08-05 16:17:12 +0800

    Make secp256k1 build on 32bit targets

--- secp256k1/libsecp256k1-config.h
+++ secp256k1/libsecp256k1-config.h
@@ -3,7 +3,7 @@
 //optimizations that any compiler we target have
 #define HAVE_BUILTIN_CLZLL 1
 #define HAVE_BUILTIN_EXPECT 1
-#define HAVE___INT128 1
+//#define HAVE___INT128 1
 
 //use GMP for bignum
 #define HAVE_LIBGMP 1
@@ -12,8 +12,10 @@
 #define USE_SCALAR_INV_NUM 1
 
 //use impls best for 64-bit
-#define USE_FIELD_5X52 1
-#define USE_SCALAR_4X64 1
+//#define USE_FIELD_5X52 1
+#define USE_FIELD_10X26 1
+//#define USE_SCALAR_4X64 1
+#define USE_SCALAR_8X32 1
 
 //enable asm
 #ifdef __x86_64__
